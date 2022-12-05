24f70bb0f064b965ec904c63dc279cdfb0e8468c
Linn Rodin <ael.rodin@gmail.com>
2022-12-05 10:43:21 +0100

    updated

--- data/simulated_database.js
+++ data/simulated_database.js
@@ -3,6 +3,12 @@ const users = []
 module.exports = users
 
 
-const products = []
+const products = [
+{ tag: "featured", articleNumber: "123456", name: "Black Coat", description: "", category: "Coats", price: 350, rating: 3, imageName: "https://win22imgstorage.blob.core.windows.net/images/black-coat.png" },
+{ tag: "featured", articleNumber: "135246", name: "Black Dress", description: "", category: "Dresses", price: 200, rating: 3, imageName: "https://win22imgstorage.blob.core.windows.net/images/black-dress.png" },
+{ tag: "featured", articleNumber: "125346", name: "White Sweater", description: "", category: "Sweater", price: 100, rating: 3, imageName: "https://win22imgstorage.blob.core.windows.net/images/white-sweater.png" },
+{ tag: "featured", articleNumber: "654321", name: "Blue Jacket", description: "", category: "Jackets", price: 400, rating: 3, imageName: "https://win22imgstorage.blob.core.windows.net/images/blue-jacket.png" },
+{ tag: "featured", articleNumber: "642531", name: "Purple Bag", description: "", category: "Bags", price: 250, rating: 3, imageName: "https://win22imgstorage.blob.core.windows.net/images/purple-bag.png" }
 
+]
 module.exports = products
