1b1f07a6303aefb7df369a5b7484fc66ee920d0e
Linn Rodin <ael.rodin@gmail.com>
2022-12-06 22:58:35 +0100

    updated

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-const port = process.env.PORT || 5000
+require('dotenv').config()
+const port = process.env.WEBAPI_PORT || 5000
+const initMongoDB = require ('./mongodb-server')
 const express = require ('express')
+const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const app = express()
+
 
 //middleware
 app.use(cors())
-app.use(express.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(express.json()) // tolkar det som att denna inte behövs i mitt fall då jag har bodyparser.
+app.use(express.urlencoded({ extended: true })) //is a method inbuilt in express to recognize the incoming Request, can parse incoming Request Object if object, with nested objects, or any type.
+app.use(bodyParser.json()) //specifically for POST Requests
 
 //routes
-const usersController = require('./controllers/usersController')
-app.use('/api/users', usersController)
 
-const formController = require('./controllers/formController')
-app.use('/api/form', formController)
+initMongoDB()
+// const usersController = require('./controllers/usersController')
+// app.use('/api/users', usersController)
 
-const productsController = require('./controllers/productsController')
-app.use('/api/products', productsController)
+// const formController = require('./controllers/formController')
+// app.use('/api/form', formController)
+
+// const productsController = require('./controllers/productsController')
+// app.use('/api/products', productsController)
 
 // initialize
 app.listen(port, () => console.log(`WebApi is running on http://localhost:${port}`))
+
